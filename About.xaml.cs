5ed575bbb27cdaa70c04b1c143f830fa7a082637
Guest free <Guest free@LAPTOP-460746DG>
2022-12-13 15:48:26 +0200

    Add project files.

---
+++ About.xaml.cs
@@ -0,0 +1,9 @@
+namespace Bodea_Alexia_Lab7;
+
+public partial class About : ContentPage
+{
+	public About()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Bodea_Alexia_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
